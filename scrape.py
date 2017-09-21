ceada033fb447fab63b0017d923d3115d6433f17
emilyzhong <emilyzhongsihua@gmail.com>
2017-09-21 10:31:29 -0700

    first commit

---
+++ scrape.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://english.berkeley.edu/course_semesters/38/printable_show/all'
+response = requests.get(url)
+html = response.content
+
+soup = BeautifulSoup(html)
+heading = soup.find('h5')
+print(soup.prettify())
