83b16cf0093962c25703a97c7fb81af516151023
emilyzhong <emilyzhongsihua@gmail.com>
2017-09-21 14:04:48 -0700

    scraping for fall 2017 + csv

--- scrape.py
+++ scrape.py
@@ -1,3 +1,4 @@
+import csv
 import requests
 from bs4 import BeautifulSoup
 
@@ -11,29 +12,37 @@ class_line = soup.find_all('hr')
 list_of_rows = []
 
 for c in class_line:
-    course_name_element = c.find_next_sibling()
-    course_logistics_element = course_name_element.find_next_sibling()
-    course_number_element = course_logistics_element.find('h4')
+    try:
+        course_name_element = c.find_next_sibling()
+        course_logistics_element = course_name_element.find_next_sibling()
+        course_number_element = course_logistics_element.find('h4')
 
+        book_list_header = course_logistics_element.find_next_sibling().get_text()
+        if book_list_header != 'Book List':
+            continue
 
-    book_list_element = course_logistics_element.find_next_sibling().find_next_sibling()
-    book_list = book_list_element.get_text().split(';')
+        book_list_element = course_logistics_element.find_next_sibling().find_next_sibling()
+        book_list = book_list_element.get_text().split(';')
 
-    for book_author in book_list:
-        book_info_row = [] # each row includes course name, number, then book and author
-        print(course_number_element)
-        book_info_row.extend([course_name_element.get_text(), course_number_element]) 
+        for author_book in book_list:
+            book_info_row = [] # each row includes course name, number, then book and author
+            book_info_row.extend([course_name_element.get_text().strip(), course_number_element.get_text().strip()]) 
 
-        try: 
-            book, author = book_author.split(':', 1)
-            # print(book, author)
-            book_info_row.append(book)
-            book_info_row.append(author)
-            
-            list_of_rows.append(book_info_row)
-        except:
-            # jank debugging
-            # print("error:", book_author)
-            1 + 1
+            try: 
+                if 'Recommended' in author_book:
+                    continue
+                author, book = author_book.split(':', 1)
+                book_info_row.append(book.strip())
+                book_info_row.append(author.strip())
+                
+                list_of_rows.append(book_info_row)
+            except:
+                # jank debugging
+                # print("error:", author_book)
+                continue
+    except:
+        continue
 
-print(list_of_rows)
+outfile = open("books.csv", "w")
+writer = csv.writer(outfile)
+writer.writerows(list_of_rows)
