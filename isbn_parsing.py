ff2570ed268bb73249ade7401ce185dcc58ef482
avniprasad <avniprasad@berkeley.edu>
2017-10-01 22:26:44 -0700

    Add files via upload
    helper functions to get information about a book's author, publishing date, description, etc.

---
+++ isbn_parsing.py
@@ -0,0 +1,49 @@
+import csv
+import requests
+from bs4 import BeautifulSoup
+
+list_of_rows = []
+
+def find(book_name):
+    book_name = book_name.lower().replace(' ', '+')
+    url = 'https://www.googleapis.com/books/v1/volumes?q=' + book_name
+    response = requests.get(url)
+    html = response.content
+    soup = BeautifulSoup(html, "html.parser")
+
+    data = response.json()
+    items = data.get('items')
+    book_dict = items[0]
+    return book_dict
+
+def author_name(book_info):
+    volume_info = book_info.get('volumeInfo')
+    return volume_info.get('authors')
+
+def book_categories(book_info):
+    volume_info = book_info.get('volumeInfo')
+    return volume_info.get('categories')
+
+def book_pages(book_info):
+    volume_info = book_info.get('volumeInfo')
+    return volume_info.get('pageCount')
+
+def book_average_rating(book_info):
+    volume_info = book_info.get('volumeInfo')
+    return volume_info.get('averageRating')
+
+def book_description(book_info):
+    volume_info = book_info.get('volumeInfo')
+    return volume_info.get('description')
+
+def book_publish_date(book_info):
+    volume_info = book_info.get('volumeInfo')
+    return volume_info.get('publishedDate')
+
+book = find('Do Androids Dream of Electric Sheep?')
+print('Author:', author_name(book))
+print('Categories:', book_categories(book))
+print('Pages:', book_pages(book))
+print('Average Rating:', book_average_rating(book))
+print('Published Data:', book_publish_date(book))
+print('Description:', book_description(book))
